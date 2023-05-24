962a3a6e10304401be1729ea6f42822f202bc428
madfelps <fmoura7@live.com>
2023-05-24 20:35:57 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     readme = fh.read()
 
-setup(name='pacotepypi',
+setup(name='pycalc',
     version='0.0.1',
-    url='https://github.com/caiocarneloz/pacotepypi',
+    url='https://github.com/madfelps/py-calc',
     license='MIT License',
-    author='Caio Carneloz',
+    author='Felipe Moura',
     long_description=readme,
     long_description_content_type="text/markdown",
-    author_email='caiocarneloz@gmail.com',
+    author_email='fmoura7@live.com',
     keywords='Pacote',
     description=u'Exemplo de pacote PyPI',
-    packages=['pacotepypi'],
+    packages=['pycalc'],
     install_requires=['numpy'],)
